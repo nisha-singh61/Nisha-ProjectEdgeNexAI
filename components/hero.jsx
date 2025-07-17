05178974c162efdc0c15e6b90abd069c94558636
Nisha Singh <nishakuswaha2002@gmail.com>
2025-07-17 13:00:28 +0530

    main image optimize - LCP

--- components/hero.jsx
+++ components/hero.jsx
@@ -49,7 +49,7 @@ const HeroSection = () => {
         <div className="hero-image-wrapper mt-5 md:mt-0">
           <div ref={imageRef} className="hero-image">
             <Image
-              src="/banner4.png"
+              src="/banner5.png"
               width={1280}
               height={720}
               alt="Dashboard Preview"
